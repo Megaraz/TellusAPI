c6d238da48b667c572518705ad464b2ccbc76b2e
Rasmus Läckberg <Rasmus-Lackberg@hotmail.com>
2024-12-01 20:01:52 +0100

    .

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Test;
+﻿using TellusAPI;
+namespace Test;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        
+
+        Tellus tellus = new Tellus(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Tellus;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+        Kund kund = new Kund
+            (
+                "19910410-5235",
+                "Rasmus",
+                "Läckberg"
+            );
+
+        tellus.AddKund(kund);
     }
 }
