362ec2868c31968265a554dbb50a34073d924b9d
Megaraz <rasmus-lackberg@hotmail.com>
2024-12-01 11:58:38 +0100

    lade till Kundrelaterade tabeller

--- TellusAPI/SQLCode.sql
+++ TellusAPI/SQLCode.sql
@@ -6,28 +6,34 @@ go
 create database Tellus
 go
 
+use Tellus
+go
+
 create table Kunder
 (
 	ID int identity(1001, 1) primary key,
 	Personnr nvarchar(13) null,
 	Förnamn nvarchar(32) not null, 
 	Efternamn nvarchar(32) not null,
-	Födelseår int(4) not null
+	Födelseår int not null,
 
 )
+go
 
-create table K2K
+create table Kontaktuppgifter
+(
+	ID int identity primary key,
+	Kontakttyp nvarchar(16) not null,
+	Kontaktuppgift nvarchar(64) not null,
+		
+)
+go
+
+create table Kund2Kontakt
 (
 	ID int identity primary key,
 	KunderID int references Kunder(ID) not null,
 	KontaktuppgifterID int references Kontaktuppgifter(ID) not null,
 	unique(KunderID, KontaktuppgifterID)
 )
-
-create table Kontaktuppgifter
-(
-	ID int identity primary key,
-	Adress nvarchar(
-	
-)
-
+go
